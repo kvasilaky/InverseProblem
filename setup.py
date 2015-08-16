67beb550be4daea5a170828b990158c46e027ab2
Katya Vasilaky <knv4@columbia.edu>
2015-08-16 11:02:02 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='InverseProblem',
+      version='1.0',
+      description='Iterative Approach to using Tikhonov Regularizaiton for inverting a matrix',
+      url='http://github.com/kathrynthegreat/InverseProblem',
+      author='Kathryn The Great',
+      author_email='knv4@columbia.edu',
+      license='Vasilaky',
+      packages=['InverseProblem'],
+      zip_safe=False)
